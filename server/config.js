ccc9e08777aa9c9e520b9cb7ce9d09bfa883fa61
mattNov <matvej.novik@gmail.com>
2022-09-07 17:59:39 +0300

    init

---
+++ server/config.js
@@ -0,0 +1,4 @@
+export const ALLOWED_ORIGIN = 'http://localhost:3000';
+// адрес БД
+export const MONGODB_URI =
+  'mongodb+srv://lockdur:Hu7MxcOAQPfQ98xp@cluster0.m3yefck.mongodb.net/?retryWrites=true&w=majority';
